84b0659e561224b2c810a503579875c8bea2b6c8
NguyenHuyHoang230499 <103480136+NguyenHuyHoang230499@users.noreply.github.com>
2022-04-25 10:12:07 +0700

    Khoi tao dal

--- src/java/DAL/ParentDAO.java
+++ src/java/DAL/ParentDAO.java
@@ -4,10 +4,27 @@
  */
 package DAL;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Parent;
+
 /**
  *
  * @author senan
  */
 public class ParentDAO extends DBContext{
-    
+    public Parent getParent(int id){
+        String sql = "select * from Parent p\n" +
+"inner join Student s\n" +
+"on p.StudentId = s.StudentId\n" +
+"where p.ParentId=?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(ParentDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
